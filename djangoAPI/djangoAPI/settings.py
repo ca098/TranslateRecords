eafeff26ef46ed50dddea776dce7e12b09877bcc
connor <cradams@hotmail.co.uk>
2020-08-02 16:30:29 +0100

    Fix
    Fixing issue

--- djangoAPI/djangoAPI/settings.py
+++ djangoAPI/djangoAPI/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uf7o73eakxkf&ae7x7-sk@-@7x&x$a7^=^)@$alt0m4rt*w%(c'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
